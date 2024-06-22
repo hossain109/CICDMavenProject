b8824b36c4e4b3cba67c12855bdc9a1f74a36b6b
Mohammad hossain <mohammadhossain109@gmail.com>
2024-06-22 16:38:38 +0200

    maven with jenkins cicd

---
+++ src/test/java/Apps/AppTest.java
@@ -0,0 +1,32 @@
+package Apps;
+
+import static org.testng.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+
+
+public class AppTest
+{
+
+    private static final int myVar = 10;
+    private static final String HELLO = "hello";
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithTrue1()
+    {
+        assertTrue( true );
+    }
+}
