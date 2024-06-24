a5666cb39ed21324bc8038575822b8c183d5bd8c
Mohammad hossain <mohammadhossain109@gmail.com>
2024-06-24 11:52:11 +0200

    modification

--- src/main/java/DevOpsDemo/Apps/App.java
+++ src/main/java/DevOpsDemo/Apps/App.java
@@ -20,6 +20,7 @@ public class App
         Calculator calculator = new Calculator();
         
         System.out.println("Resultat des saisaies (sum) : "+calculator.sum(number1, number2));
+         System.out.println("Resultat des saisaies (sum) : "+calculator.sum(number2, number2));
         System.out.println("Resultat des saisaies (subtract) : "+calculator.subtract(number1, number2));
         System.out.println("Resultat des saisaies (multiply) : "+calculator.multiply(number1, number2));
         System.out.println("Resultat des saisaies (divide) : "+calculator.divide(number1, number2));
