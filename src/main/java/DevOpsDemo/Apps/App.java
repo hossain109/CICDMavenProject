d7c44dbbf695532a9d975938a0b21ca3588f4a1c
hossain109 <mohammadhossain109@gmail.com>
2024-07-31 01:18:40 +0200

    jenkinsfile modification

--- src/main/java/DevOpsDemo/Apps/App.java
+++ src/main/java/DevOpsDemo/Apps/App.java
@@ -25,5 +25,6 @@ public class App
         System.out.println("Resultat des saisaies (multiply) : "+calculator.multiply(number1, number2));
         System.out.println("Resultat des saisaies (divide) : "+calculator.divide(number1, number2));
         System.out.println("Resultat des saisaies (divide) : "+calculator.divide(number2, number2));
+        System.out.println("Resultat des saisaies (subtract) : "+calculator.subtract(number1, number2));
     }
 }
